14b8e8ce6360b25d8e1ce359c96064bdd86e02ce
louvetg <Gaspard.louvet@phelma.grenoble-inp.fr>
2016-11-23 14:00:03 +0100

    voili voilou
    Tu avais oublié de changer le nb de prim

--- primitive.h
+++ primitive.h
@@ -9,7 +9,7 @@ extern "C" {
 #include "object.h"
 #include "eval.h"
 
-#define NB_PRIM 1
+#define NB_PRIM 29
 
 void init_tab_prim(char tab_form[NB_PRIM][STRLEN]);
 void init_add_tab_prim(adress tab_add_form[NB_PRIM]);
